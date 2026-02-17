9663f8af9fa465b530ea5f65f74e35e11270939a
Udar Studio <113240006+udarstudio@users.noreply.github.com>
2026-02-17 22:36:18 +0200

    Add favicon and web manifest files
    - Added favicon images: favicon-16x16.png, favicon-32x32.png, and favicon.ico to the public directory.
    - Created a site.webmanifest file with basic configuration including app name, icons, theme color, and background color.

--- .output/public/_nuxt/B8ydhQrL.js
+++
@@ -1,1 +0,0 @@
-import{F as a,G as s,H as r,I as u,J as o}from"./ITztVqlN.js";function i(e){const t=e||s();return t?.ssrContext?.head||t?.runWithContext(()=>{if(r())return u(o)})}function x(e,t={}){const n=i(t.nuxt);if(n)return a(e,{head:n,...t})}export{x as u};
